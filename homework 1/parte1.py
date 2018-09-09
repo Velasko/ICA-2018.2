120c7585273ea9f97f3a9a8c1bc9cdb80629b930
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2018-09-09 14:00:26 -0300

    q1 e q2 prontas

--- homework 1/parte1.py
+++ homework 1/parte1.py
@@ -6,6 +6,59 @@ import getdata
 
 data = getdata.get_data('Dados\\glass.data')
 
+
+#plot de dados bruto
+sub = ['ID', 'RI', 'Na', 'Mg', 'Al', 'Si', 'k', 'Ca', 'Ba', 'Fe', 'type']
+matrix = [e for e in range(len(data) -1 )]
+
+means = []
+std = []
+median = []
+skewness = []
+
+fig, ax = plt.subplots(3, 5)
+for n, column in enumerate(data.T):
+	column = column[1:]
+
+	axis = (n//5, n%5)
+
+	ax[axis].set_title(sub[n])
+	# ax[axis].hist(matrix, weights=column[1:])
+	ax[axis].hist(column)
+
+	means.append(column.mean())
+	median.append( np.median(column) )
+	std.append( column.std() )
+
+	skewness_eq = sum([ (xi - means[n])**3 for xi in column[1:] ])/((len(column) - 2)*std[n]**(3))
+	skewness.append(skewness_eq)
+
+
+sub = sub[1:]
+for n in range(1, 5):
+	plt.setp(ax[2, n], xticks=range(len(sub)), xticklabels=sub)
+bins = np.arange(len(sub)) - 0.5
+matrix = [e for e in range(len(sub))]
+
+ax[2, 1].set_title('Médias')
+#ax[2, 1].hist(matrix, weights=means, bins=bins)
+ax[2, 1].stem(means[1:])
+
+ax[2, 2].set_title('Desvio Padrão')
+#ax[2, 2].hist(matrix, weights=std, bins=bins)
+ax[2, 2].stem(std[1:])
+
+ax[2, 3].set_title('Medianas')
+#ax[2, 3].hist(matrix, weights=median, bins=bins)
+ax[2, 3].stem(median[1:])
+
+ax[2, 4].set_title('Assimetria')
+#ax[2, 4].hist(matrix, weights=skewness, bins=bins)
+ax[2, 4].stem(skewness[1:])
+
+plt.show()
+
+
 #calculating for general
 means = [ column[1:].mean() for column in data.T ]
 median = [ np.median(column[1:]) for column in data.T ]
@@ -19,90 +72,3 @@ for n, column in enumerate(data.T):
 	skewness_eq = sum([ (xi - means[n])**3 for xi in column[1:] ])/((len(column) - 2)*std[n]**(3))
 	skewness.append(skewness_eq)
 
-# Calculating by glass type
-means = []
-std = []
-median = []
-skewness = []
-
-for filter in range(1, 8):
-
-	#because there is no data on the 4th type
-	if filter == 4: 
-		means.append([np.nan]*10)
-		std.append([np.nan]*10)
-		median.append([np.nan]*10)
-		skewness.append([np.nan]*10)
-		continue
-
-	rows = np.where(data[:,-1] == filter)
-
-	means.append([])
-	std.append([])
-	median.append([])
-	skewness.append([])
-
-	for n, column in enumerate(data[rows].T):
-		if n == 0 : continue
-		elif n != 1:
-			column /= 100
-
-		means[-1].append(column.mean())
-		std[-1].append(column.std(dtype=np.float64))
-		median[-1].append(np.median(column))
-
-		try:
-			skewness_eq = sum([ (xi - means[-1][-1])**3 for xi in column ])/((len(column) - 1)*std[-1][-1]**3)
-			skewness[-1].append(skewness_eq)
-		except ZeroDivisionError:
-			skewness[-1].append(np.nan)
-
-
-sub = ['ID', 'RI', 'Na', 'Mg', 'Al', 'Si', 'k', 'Ca', 'Ba', 'Fe', 'type'][1:]
-labels = ["Tipo {}".format(e+1) for e in range(7)]
-override = {'fontsize' : 'large', 'horizontalalignment' : 'center'}
-
-fig, ax = plt.subplots(2, 2)
-plt.setp(ax, xticks=range(len(sub)), xticklabels=sub)
-bins = np.arange(len(sub)) - 0.5
-
-# ax.ylabel('Concentração (%)')
-plt.xlabel('Substâncias', override)
-
-matrix = [[ e for e in range(len(means[0])) ]] * len(means)
-
-ax[0, 0].set_title('Médias')
-ax[0, 0].hist(matrix, weights=means, bins=bins)
-
-ax[0, 1].set_title('Desvio Padrão')
-ax[0, 1].hist(matrix, weights=std, bins=bins)
-
-ax[1, 0].set_title('Medianas')
-ax[1, 0].hist(matrix, weights=median, bins=bins)
-
-ax[1, 1].set_title('Assimetria')
-ax[1, 1].hist(matrix, weights=skewness, bins=bins)
-
-plt.legend(labels)
-plt.show()
-
-
-# #plot de dados bruto
-# fig, ax = plt.subplots(3, 5)
-# for n, column in enumerate(data[rows].T):
-# 	if n == 0: continue
-# 	show = column
-
-# 	if column[0] == 5: print(show)
-
-# 	axis = (n//5, n%5)
-
-# 	ax[axis].set_title(sub[n-1])
-# 	#ax[axis].hist(show)
-# 	ax[axis].stem(show)#, histtype='barstacked')
-
-# plt.stem(show)#, label=sub, align='left', histtype='barstacked', rwidth=700000)
-# 	#range(len(show)), weights=
-
-
-#plt.show()
---
+++ homework 1/parte2.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+
+import getdata
+
+data = getdata.get_data('Dados\\glass.data')
+
+# Calculating by glass type
+means = []
+std = []
+median = []
+skewness = []
+
+sub = ['ID', 'RI', 'Na', 'Mg', 'Al', 'Si', 'k', 'Ca', 'Ba', 'Fe', 'type'][1:-1]
+labels = ["Tipo {}".format(e+1) for e in range(7)]
+override = {'fontsize' : 'large', 'horizontalalignment' : 'center'}
+
+for filter in range(1, 8):
+
+	#because there is no data on the 4th type
+	if filter == 4:
+		size = len(sub)
+		means.append([np.nan]*size)
+		std.append([np.nan]*size)
+		median.append([np.nan]*size)
+		skewness.append([np.nan]*size)
+		continue
+
+	rows = np.where(data[:,-1] == filter)
+
+	means.append([])
+	std.append([])
+	median.append([])
+	skewness.append([])
+
+	fig, ax = plt.subplots(3, 5)
+	for n, column in enumerate(data[rows].T):
+		if n == 0 or n == 10 : continue
+		elif n != 1:
+			column /= 100
+
+		means[-1].append(column.mean())
+		std[-1].append(column.std(dtype=np.float64))
+		median[-1].append(np.median(column))
+
+		try:
+			skewness_eq = sum([ (xi - means[-1][-1])**3 for xi in column ])/((len(column) - 1)*std[-1][-1]**3)
+			skewness[-1].append(skewness_eq)
+		except ZeroDivisionError:
+			skewness[-1].append(np.nan)
+
+		#plotting raw data from current type
+		n -= 1
+		axis = (n//5, n%5)
+		ax[axis].set_title(sub[n])
+		ax[axis].hist(column)
+
+	for n in range(4):
+		plt.setp(ax[2, n], xticks=range(len(sub)), xticklabels=sub)
+
+	ax[2, 0].set_title('Médias')
+	ax[2, 0].stem(means[-1])
+
+	ax[2, 1].set_title('Desvio Padrão')
+	ax[2, 1].stem(std[-1])
+
+	ax[2, 2].set_title('Medianas')
+	ax[2, 2].stem(median[-1])
+
+	ax[2, 3].set_title('Assimetria')
+	ax[2, 3].stem(skewness[-1])
+
+	plt.suptitle('Plots sobre o Tipo {}'.format(filter))
+#	plt.show()
+
+#sub = ['ID', 'RI', 'Na', 'Mg', 'Al', 'Si', 'k', 'Ca', 'Ba', 'Fe', 'type'][1:-1]
+fig, ax = plt.subplots(2, 2)
+plt.setp(ax, xticks=range(len(sub)), xticklabels=sub)
+bins = np.arange(len(sub)) - 0.5
+
+matrix = [[ e for e in range(len(means[0])) ]] * len(means)
+
+ax[0, 0].set_title('Médias')
+ax[0, 0].hist(matrix, weights=means, bins=bins)
+
+ax[0, 1].set_title('Desvio Padrão')
+ax[0, 1].hist(matrix, weights=std, bins=bins)
+
+ax[1, 0].set_title('Medianas')
+ax[1, 0].hist(matrix, weights=median, bins=bins)
+
+ax[1, 1].set_title('Assimetria')
+ax[1, 1].hist(matrix, weights=skewness, bins=bins)
+
+plt.legend(labels)
+plt.suptitle('Plots sobre todos os tipos')
+plt.show()
